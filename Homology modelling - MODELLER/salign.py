e50ab09b9e74493083f7a341b99c75bdea2987fa
Ariel <ariel.tijman@gmail.com>
2021-09-07 13:56:48 -0300

    Upload

---
+++ Homology modelling - MODELLER/salign.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Align PDBs for construction of a multiple template homology model.
+
+from modeller import *
+
+log.verbose()
+env = Environ()
+env.io.atom_files_directory = ['.', '../atom_files/']
+
+aln = Alignment(env)
+for (code, chain) in (('4ce5', 'A'), ('4cmd', 'A')):                                     # Input structures
+    mdl = Model(env, file=code, model_segment=('FIRST:'+chain, 'LAST:'+chain))
+    aln.append_model(mdl, atom_files=code, align_codes=code+chain)
+
+for (weights, write_fit, whole) in (((1., 0., 0., 0., 1., 0.), False, True),
+                                    ((1., 0.5, 1., 1., 1., 0.), False, True),
+                                    ((1., 1., 1., 1., 1., 0.), True, False)):
+    aln.salign(rms_cutoff=3.5, normalize_pp_scores=False,
+               rr_file='$(LIB)/as1.sim.mat', overhang=30,
+               gap_penalties_1d=(-450, -50),
+               gap_penalties_3d=(0, 3), gap_gap_score=0, gap_residue_score=0,
+               dendrogram_file='TAs.tree',                                               # Tree
+               alignment_type='tree', # If 'progresive', the tree is not
+                                      # computed and all structues will be
+                                      # aligned sequentially to the first
+               feature_weights=weights, # For a multiple sequence alignment only
+                                        # the first feature needs to be non-zero
+               improve_alignment=True, fit=True, write_fit=write_fit,
+               write_whole_pdb=whole, output='ALIGNMENT QUALITY')
+
+aln.write(file='TAs.pap', alignment_format='PAP')                                        # Alignment PAP
+aln.write(file='TAs.ali', alignment_format='PIR')                                        # Alignment PIR
+
+aln.salign(rms_cutoff=1.0, normalize_pp_scores=False,
+           rr_file='$(LIB)/as1.sim.mat', overhang=30,
+           gap_penalties_1d=(-450, -50), gap_penalties_3d=(0, 3),
+           gap_gap_score=0, gap_residue_score=0, dendrogram_file='TAs.tree',             # Tree
+           alignment_type='progressive', feature_weights=[0]*6,
+           improve_alignment=False, fit=False, write_fit=True,
+           write_whole_pdb=False, output='QUALITY')
+           
