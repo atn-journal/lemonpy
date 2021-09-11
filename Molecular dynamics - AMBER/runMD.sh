876caf49a51408f983c9d96aee3f4beab73d4d84
Ariel <ariel.tijman@gmail.com>
2021-09-11 17:03:10 -0300

    Add residues input for analysis

--- Molecular dynamics - AMBER/runMD.sh
+++ Molecular dynamics - AMBER/runMD.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Define names
-top='basename_of_prmtop'
-inp='basename_of_input_coord'
-out='basename_of_output_trajectory'
+# Define basenames
+top='prmtop'
+inp='rst'
+out='trajectory'
+res='residues'
 
 # RUN MD
 time pmemd.cuda -O -i md.in -o ${out}.out -p ${top}.prmtop -c ${inp}.rst -r ${out}.rst -x ${out}.nc
 
 # Process trajectory
 strip-traj.sh -p ${top}.prmtop -t ${out}.nc
-analyze-traj.sh -p ${top}.prmtop -t ${out}_dry.nc
+analyze-traj.sh -p ${top}.prmtop -t ${out}_dry.nc -r ${res}
 reduce-traj.sh -p ${top}.prmtop -t ${out}_dry.nc
 
 send-email.py "Terminó la corrida ${out} en MicroMol"
@@ -25,7 +26,7 @@ time pmemd.cuda -O -i md.in -o ${out}.out -p ${top}.prmtop -c ${inp}.rst -r ${ou
 
 # Process trajectory
 strip-traj.sh -p ${top}.prmtop -t ${out}.nc
-analyze-traj.sh -p ${top}.prmtop -t ${out}_dry.nc
+analyze-traj.sh -p ${top}.prmtop -t ${out}_dry.nc -r ${res}
 reduce-traj.sh -p ${top}.prmtop -t ${out}_dry.nc
 
 send-email.py "Terminó la corrida ${out} en MicroMol"
