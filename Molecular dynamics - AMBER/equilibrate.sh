988dae391861e166abbf6c2fdd5e18915f0945c7
Ariel <ariel.tijman@gmail.com>
2021-09-16 10:31:38 -0300

    Initial commit

---
+++ Molecular dynamics - AMBER/equilibrate.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Usage: ./equilibrate.sh -p {prmtop} -c {coordinates} -t {temperature}
+# Temperature must be an integer provided in K.
+
+# Get topology, coordinates and temperature
+while getopts :p:c:t: flag
+do
+    case "${flag}" in
+        p) prmtop=$(basename ${OPTARG} .prmtop);;
+        c) coord=$(basename ${OPTARG} .rst);;
+        t) temp=${OPTARG};;
+    esac
+done
+
+# Create equilibration input file
+cat << EOF > eq.in
+Equilibrating the system during 5000 ps at constant T,
+P = 1 bar and coupling = 0.2 ps.
+  &cntrl
+    ntx=5,irest=1,
+    t=0.0,dt=0.002,nstlim=250000,
+    temp0=${temp}.0,ntt=3,ig=-1,gamma_ln=5.0,
+    pres0=1.0,ntp=1,barostat=1,taup=0.2,
+    ntc=2,ntf=2,
+    nscm=5000,
+    cut=9.0,
+    ntpr=500,ntwr=125000,ntwx=500,
+  /
+
+EOF
+
+# Redirect stdout and stderr to log
+exec > >(tee eq.log) 2>&1
+
+echo topology=$prmtop
+echo coordinates=$coord
+echo temperature=$temp
+
+# Equilibrate the system
+pmemd.cuda -O -i eq.in -o ${prmtop}_eq.out -p ${prmtop}.prmtop -c ${coord}.rst -r ${prmtop}_eq.rst -x ${prmtop}_eq.nc
+
+echo -e "\n##################################################"
+echo "Equilibration took $((${SECONDS}/60)) min"
+echo -e "##################################################\n"
+
+#Display results in VMD
+vmd ${prmtop}.prmtop ${prmtop}_eq.nc
---
+++ Molecular dynamics - AMBER/heat.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Usage: ./heat.sh -p {prmtop} -c {coordinates} -r {residues} -t {temperature}
+# Temperature must be an integer provided in K.
+
+# Get topology, coordinates, number of residues and target temperature
+while getopts :p:c:r:t: flag
+do
+    case "${flag}" in
+        p) prmtop=$(basename ${OPTARG} .prmtop);;
+        c) coord=$(basename ${OPTARG} .rst);;
+        r) res=${OPTARG};;
+        t) temp=${OPTARG};;
+    esac
+done
+
+# Create heating input file
+cat << EOF > heat.in
+Heating the system with 5 kcal/mol restraints on PROTEIN,
+V = const.
+  &cntrl
+    t=0.0,dt=0.002,nstlim=50000,
+    ntr=1,restraintmask=':1-${res}',restraint_wt=5.0,
+    tempi=100.0,ntt=3,ig=-1,gamma_ln=5.0,
+    ntc=2,ntf=2,
+    nscm=5000,
+    cut=9.0,
+    ntpr=500,ntwr=0,ntwx=500,
+    nmropt=1,
+  /
+  &wt
+    type='TEMP0',
+    istep1=0,istep2=5000,
+    value1=100.0,value2=${temp}.0,
+  /
+  &wt
+    type='TEMP0',
+    istep1=5001,istep2=50000,
+    value1=${temp}.0,value2=${temp}.0,
+  /
+  &wt type='END',
+  /
+
+EOF
+
+# Redirect stdout and stderr to log
+exec > >(tee heat.log) 2>&1
+
+echo topology=$prmtop
+echo coordinates=$coord
+echo residues=$res
+echo temperature=$temp
+
+# Heat the system
+pmemd.cuda -O -i heat.in -o ${prmtop}_heat.out -p ${prmtop}.prmtop -c ${coord}.rst -r ${prmtop}_heat.rst -x ${prmtop}_heat.nc -ref ${coord}.rst
+
+echo -e "\n##################################################"
+echo "Heating took $((${SECONDS}/60)) min"
+echo -e "##################################################\n"
+
+#Display results in VMD
+vmd ${prmtop}.prmtop ${prmtop}_heat.nc
