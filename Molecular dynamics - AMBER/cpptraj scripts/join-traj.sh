f703a70f135bce4f525966a049722193a7277773
Ariel <ariel.tijman@gmail.com>
2021-09-16 20:11:45 -0300

    Display all trajectories in log

--- Molecular dynamics - AMBER/cpptraj scripts/join-traj.sh
+++ Molecular dynamics - AMBER/cpptraj scripts/join-traj.sh
@@ -2,6 +2,9 @@
 
 # Usage: ./join-traj.sh -p {prmtop} -t {first_trajectory} -j {trajectory_to_add}
 
+# Redirect stdout and stderr to log
+exec > >(tee join.log) 2>&1
+
 # Get topology and trajectories to join
 while getopts :p:t:j: flag
 do
@@ -10,16 +13,19 @@ do
 cat << EOF > join.in
 parm ${prmtop}.prmtop
 EOF
+echo topology=$prmtop
 ;;
         t) nc1=$(basename ${OPTARG} .nc)
 cat << EOF >> join.in
 trajin ${nc1}.nc
 EOF
+echo first trajectory=$nc1
 ;;
         j) nc2=$(basename ${OPTARG} .nc)
 cat << EOF >> join.in
 trajin ${nc2}.nc
 EOF
+echo add trajectory=$nc2
 ;;
     esac
 done
@@ -28,15 +34,8 @@ done
 cat << EOF >> join.in
 trajout ${nc2}_join.nc netcdf
 go
-    
+
 EOF
-
-# Redirect stdout and stderr to log
-exec > >(tee join.log) 2>&1
-
-echo topology=$prmtop
-echo first trajectory=$nc1
-echo second trajectory=$nc2
 
 # Join trajectories
 cpptraj -i join.in
