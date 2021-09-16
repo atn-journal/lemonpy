22b9a5d09f1904171786c7e057efabc91b50a003
Ariel <ariel.tijman@gmail.com>
2021-09-16 10:32:23 -0300

    Modify min.in

--- Molecular dynamics - AMBER/minimize.sh
+++ Molecular dynamics - AMBER/minimize.sh
@@ -13,91 +13,46 @@ done
 
 # Create minimization input files
 cat << EOF > min1.in
-Minimizing the system 
-with 25 kcal/mol restraints 
-on PROTEIN, 500 steps of steepest
-descent and 2000 of 
-conjugated gradient
- &cntrl
- imin=1,
- ntx=1,
- irest=0,
- ntpr=50, 
- ntf=1, 
- ntb=1,
- cut=9.0,
- nsnb=10,
- ntr=1, 
- maxcyc=2500,
- ncyc=2000,
- ntmin=1,
- restraintmask=':1-${res}',
- restraint_wt=25.0,
- &end
- &ewald
-   ew_type = 0, skinnb = 1.0,
- &end
+Minimizing the system with 25 kcal/mol restraints on PROTEIN,
+2000 steps of steepest descent and 500 of conjugated gradient.
+  &cntrl
+    imin=1,ntmin=1,ncyc=2000,maxcyc=2500,
+    ntr=1,restraintmask=':1-${res}',restraint_wt=25.0,
+    cut=9.0,
+    nsnb=10,
+    ntpr=50,
+  /
 
 EOF
 
 cat << EOF > min2.in
-Minimizing the system 
-with 15 kcal/mol restraints 
-on PROTEIN, 500 steps of steepest
-descent and 2000 of 
-conjugated gradient
- &cntrl
- imin=1,
- ntx=1,
- irest=0,
- ntpr=50, 
- ntf=1, 
- ntb=1,
- cut=9.0,
- nsnb=10,
- ntr=1, 
- maxcyc=2500,
- ncyc=2000,
- ntmin=1,
- restraintmask=':1-${res}',
- restraint_wt=15.0,
- &end
- &ewald
-   ew_type = 0, skinnb = 1.0,
- &end
+Minimizing the system with 25 kcal/mol restraints on PROTEIN,
+2000 steps of steepest descent and 500 of conjugated gradient.
+  &cntrl
+    imin=1,ntmin=1,ncyc=2000,maxcyc=2500,
+    ntr=1,restraintmask=':1-${res}',restraint_wt=15.0,
+    cut=9.0,
+    nsnb=10,
+    ntpr=50,
+  /
 
 EOF
 
 cat << EOF > min3.in
-Minimizing the system 
-with 5 kcal/mol restraints 
-on PROTEIN, 500 steps of steepest
-descent and 2000 of 
-conjugated gradient
- &cntrl
- imin=1,
- ntx=1,
- irest=0,
- ntpr=50, 
- ntf=1, 
- ntb=1,
- cut=9.0,
- nsnb=10,
- ntr=1, 
- maxcyc=2500,
- ncyc=2000,
- ntmin=1,
- restraintmask=':1-${res}',
- restraint_wt=5.0,
- &end
- &ewald
-   ew_type = 0, skinnb = 1.0,
- &end
+Minimizing the system with 25 kcal/mol restraints on PROTEIN,
+2000 steps of steepest descent and 500 of conjugated gradient.
+  &cntrl
+    imin=1,ntmin=1,ncyc=2000,maxcyc=2500,
+    ntr=1,restraintmask=':1-${res}',restraint_wt=5.0,
+    cut=9.0,
+    nsnb=10,
+    ntpr=50,
+  /
 
 EOF
 
 # Redirect stdout and stderr to log
-exec > >(tee minimize.log) 2>&1
+exec > >(tee min.log) 2>&1
 
 echo topology=$prmtop
 echo residues=$res
@@ -107,7 +62,9 @@ pmemd.cuda -O -i min1.in -o ${prmtop}_min1.out -p ${prmtop}.prmtop -c ${prmtop}.
 pmemd.cuda -O -i min2.in -o ${prmtop}_min2.out -p ${prmtop}.prmtop -c ${prmtop}_min1.rst -r ${prmtop}_min2.rst -ref ${prmtop}.inpcrd -inf ${prmtop}_min2.mdinfo
 pmemd.cuda -O -i min3.in -o ${prmtop}_min3.out -p ${prmtop}.prmtop -c ${prmtop}_min2.rst -r ${prmtop}_min3.rst -ref ${prmtop}.inpcrd -inf ${prmtop}_min3.mdinfo
 
-echo -e "\n##################################################";echo "Minimization took ${SECONDS} s"; echo -e "##################################################\n"
+echo -e "\n##################################################"
+echo "Minimization took ${SECONDS} s"
+echo -e "##################################################\n"
 
 #Display results in VMD
 vmd ${prmtop}.prmtop -netcdf ${prmtop}_min1.rst -netcdf ${prmtop}_min2.rst -netcdf ${prmtop}_min3.rst
