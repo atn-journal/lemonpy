c8ed22309c98f0ca9ad1b5efe3896941fc7f1150
Ariel <ariel.tijman@gmail.com>
2021-12-30 08:29:27 -0300

    for loops

--- Molecular dynamics - AMBER/pmemd scripts/cluster-batch.sh
+++ Molecular dynamics - AMBER/pmemd scripts/cluster-batch.sh
@@ -3,26 +3,30 @@
 ID='d6'
 dependency=5714561
 ns_in=2700
+ns_slurm=100
 
-ns_out=$(($ns_in+100))
+ns_out=$(($ns_in+$ns_slurm))
 file="$ID-${ns_in}ns"
 output="$ID-${ns_out}ns"
 
 sed -r "s/#SBATCH --job-name=.*/#SBATCH --job-name=$output/" $file.slurm > $output.slurm
 sed -r -i "s/#*SBATCH --dependency=afterok:.*/#SBATCH --dependency=afterok:$dependency/" $output.slurm
-sed -r -i "s/^ns_in=$(($ns_in-100))/ns_in=$ns_in/" $output.slurm
+sed -r -i "s/^ns_in=$(($ns_in-$ns_slurm))/ns_in=$ns_in/" $output.slurm
 
 dependency=$(sbatch $output.slurm | awk '{print $4}')
 
 #######################################################################################################################
 
-ns_in=$ns_out
-ns_out=$(($ns_in+100))
-file="$ID-${ns_in}ns"
-output="$ID-${ns_out}ns"
+for((i=0;i<1;i++));
+do
+    ns_in=$ns_out
+    ns_out=$(($ns_in+$ns_slurm))
+    file="$ID-${ns_in}ns"
+    output="$ID-${ns_out}ns"
 
-sed -r "s/#SBATCH --job-name=.*/#SBATCH --job-name=$output/" $file.slurm > $output.slurm
-sed -r -i "s/#*SBATCH --dependency=afterok:.*/#SBATCH --dependency=afterok:$dependency/" $output.slurm
-sed -r -i "s/^ns_in=$(($ns_in-100))/ns_in=$ns_in/g" $output.slurm
+    sed -r "s/#SBATCH --job-name=.*/#SBATCH --job-name=$output/" $file.slurm > $output.slurm
+    sed -r -i "s/#*SBATCH --dependency=afterok:.*/#SBATCH --dependency=afterok:$dependency/" $output.slurm
+    sed -r -i "s/^ns_in=$(($ns_in-$ns_slurm))/ns_in=$ns_in/g" $output.slurm
 
-dependency=$(sbatch $output.slurm | awk '{print $4}')
+    dependency=$(sbatch $output.slurm | awk '{print $4}')
+done
