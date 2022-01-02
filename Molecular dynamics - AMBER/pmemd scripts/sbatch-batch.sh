153eba2f167ad42142c7746e5a9baaa7bb30f82c
Ariel <ariel.tijman@gmail.com>
2022-01-01 22:19:48 -0300

    Improve for loop

--- Molecular dynamics - AMBER/pmemd scripts/sbatch-batch.sh
+++ Molecular dynamics - AMBER/pmemd scripts/sbatch-batch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ID='d6'
-dependency=5714561
-ns_in=2700
-ns_slurm=100
+ID=''
+dependency=
+ns_in=
+ns_slurm=
+up_to=
 
 ns_out=$(($ns_in+$ns_slurm))
 file="$ID-$(printf "%04d" $ns_in)ns"
@@ -17,7 +18,7 @@ dependency=$(sbatch $output.slurm | awk '{print $4}')
 
 #######################################################################################################################
 
-for((i=0;i<1;i++));
+for i in $(eval echo {$(($ns_slurm*2))..$(($up_to-$ns_slurm))..$ns_slurm})
 do
     ns_in=$ns_out
     ns_out=$(($ns_in+$ns_slurm))
